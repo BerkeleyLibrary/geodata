a42aad38798f47f2c0919b23823c055d34303a96
yzhoubk <30507097+yzhoubk@users.noreply.github.com>
2023-11-29 14:57:37 -0800

    Update 4.1 (#4)
    * home page
    
    * remove tools

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -306,18 +306,18 @@ class CatalogController < ApplicationController
     # Tools from Blacklight
     config.add_results_collection_tool(:sort_widget)
     config.add_results_collection_tool(:per_page_widget)
-    config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    # config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_show_tools_partial(:citation)
-    config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
-    config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
+    # config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
+    # config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
 
     # Custom tools for GeoBlacklight
     config.add_show_tools_partial :metadata, if: proc { |_context, _config, options|
                                                    options[:document] && (Settings.METADATA_SHOWN & options[:document].references.refs.map(&:type).map(&:to_s)).any?
                                                  }
-    config.add_show_tools_partial :carto, partial: 'carto', if: proc { |_context, _config, options|
-                                                                  options[:document] && options[:document].carto_reference.present?
-                                                                }
+    # config.add_show_tools_partial :carto, partial: 'carto', if: proc { |_context, _config, options|
+    #                                                               options[:document] && options[:document].carto_reference.present?
+    #                                                             }
     config.add_show_tools_partial :arcgis, partial: 'arcgis', if: proc { |_context, _config, options|
                                                                     options[:document] && options[:document].arcgis_urls.present?
                                                                   }
