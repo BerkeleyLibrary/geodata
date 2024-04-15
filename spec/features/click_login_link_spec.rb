516e072a84d65ff0c1ed4a56e5b50088e4d21728
zhouyu <zhouyu@berkeley.edu>
2024-04-15 15:04:32 -0700

    Add Configure from TIND-QA

--- spec/features/click_login_link_spec.rb
+++ spec/features/click_login_link_spec.rb
@@ -24,6 +24,7 @@ RSpec.feature 'Homepage', type: :feature, js: true do
   include Capybara::DSL
   scenario 'User visits the homepage' do
     # root_path = 'https://www.google.com'
+    sleep(60)
     root_path = 'http://localhost:3000/'
     visit root_path
     expect(page).to have_content('UC Berkeley GeoData Repository')
