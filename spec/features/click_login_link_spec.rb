3becf01e7d4b164ffcb650eca3d3da583a4826e2
zhouyu <zhouyu@berkeley.edu>
2024-12-06 10:33:11 -0800

    calnet login page test

--- spec/features/click_login_link_spec.rb
+++ spec/features/click_login_link_spec.rb
@@ -1,24 +1,14 @@
-# require 'spec_helper'
 require 'rails_helper'
+require 'uri'
 
-# RSpec.feature 'Clicking login link on root page', type: :feature do
-#   scenario 'User clicks login link and is redirected login page' do
-#     Rails.logger.info 'User is logged in'
-#     visit root_path
-#     click_link 'login'
-#     logger.info("PPPPP-#{user_calnet_omniauth_authorize_path}")
-#     expect(page).to have_current_path(user_calnet_omniauth_authorize_path)
-#   end
-# end
-
-# describe 'User Util Links' do
-#     it 'has link' do
-#        visit root_path
-#        click_link 'login'
-#     #    logger.info("PPPPP-#{user_calnet_omniauth_authorize_path}")
-#     #    expect(page).to have_current_path(user_calnet_omniauth_authorize_path)
-#     end
-# end
+RSpec.feature 'Clicking login link on home page', type: :feature do
+  scenario 'User clicks login link and goes redirected login page' do
+    visit root_path
+    click_on('Login')
+    url = '/cas/login?service=http%3A%2F%2Fapp.test%3A3000%2Fusers%2Fauth%2Fcalnet%2Fcallback%3Furl%3Dhttp%253A%252F%252Fapp.test%253A3000%252F'
+    expect(page).to have_current_path(url)
+  end
+end
 
 RSpec.feature 'Homepage', type: :feature, js: true do
   include Capybara::DSL
