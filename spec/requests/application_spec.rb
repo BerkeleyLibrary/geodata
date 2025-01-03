9755ff667215f73d1560f86c6ee325271f8223cf
zhouyu <zhouyu@berkeley.edu>
2025-01-03 15:04:49 -0800

    rm duplicated old request tests

--- spec/requests/application_spec.rb
+++ spec/requests/application_spec.rb
@@ -1,15 +1,15 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-# RSpec.describe "Applications", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
+# # RSpec.describe "Applications", type: :request do
+# #   describe "GET /index" do
+# #     pending "add some examples (or delete) #{__FILE__}"
+# #   end
+# # end
+
+# RSpec.describe 'View Catalog' do
+#   it 'can view Home Page' do
+#     get '/'
+#     expect(response).to be_successful
+#     expect(response.body).to include('UC Berkeley GeoData Repository')
 #   end
 # end
-
-RSpec.describe 'View Catalog' do
-  it 'can view Home Page' do
-    get '/'
-    expect(response).to be_successful
-    expect(response.body).to include('UC Berkeley GeoData Repository')
-  end
-end
--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -1,23 +1,23 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-# Get a test user, then testing Devise
+# # Get a test user, then testing Devise
 
-RSpec.describe 'Sessions' do
-  # not use this action anymore
-  # it 'can redirect Calnet login form' do
-  #   get new_user_session_path
-  #   expect(response).to have_http_status(302)
-  # end
+# RSpec.describe 'Sessions' do
+#   # not use this action anymore
+#   # it 'can redirect Calnet login form' do
+#   #   get new_user_session_path
+#   #   expect(response).to have_http_status(302)
+#   # end
 
-  it 'can redirect Calnet logout form' do
-    get destroy_user_session_path
-    expect(response).to have_http_status(302)
-  end
+#   it 'can redirect Calnet logout form' do
+#     get destroy_user_session_path
+#     expect(response).to have_http_status(302)
+#   end
 
-  # not use redirection anymore
-  # it 'redirects to auth.berkeley.edu' do
-  #   get user_calnet_omniauth_authorize_path
-  #   expect(response).to have_http_status(302)
-  #   expect(response.location).to match(%r{https://auth-test.berkeley.edu})
-  # end
-end
+#   # not use redirection anymore
+#   # it 'redirects to auth.berkeley.edu' do
+#   #   get user_calnet_omniauth_authorize_path
+#   #   expect(response).to have_http_status(302)
+#   #   expect(response.location).to match(%r{https://auth-test.berkeley.edu})
+#   # end
+# end
