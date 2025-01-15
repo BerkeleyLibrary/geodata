00587e8d02d79ce269014b2527c021a97b5bd71a
zhouyu <zhouyu@berkeley.edu>
2025-01-15 15:34:32 -0800

    optimze export file tests

--- spec/features/export_geofile_spec.rb
+++ spec/features/export_geofile_spec.rb
@@ -4,32 +4,16 @@ require 'fileutils'
 require_relative '../support/shared_examples/link_spec'
 
 RSpec.describe 'File Download' do
-  # let(:download_dir) { '/opt/app/tmp/cache/downloads' }
+  let(:download_dir) { '/opt/app/tmp/cache/downloads' }
 
-  # before do
-  #   Dir.mkdir(download_dir) unless File.exist?(download_dir)
-  #   visit 'catalog/berkeley-s7038h'
-  #   find('#downloads-button').click
-  # end
+  before do
+    FileUtils.mkdir_p(download_dir)
+    visit 'catalog/berkeley-s7038h'
+    find('#downloads-button').click
+  end
 
-  it_behaves_like 'export geofile to local',  'berkeley-s7038h-shapefile.zip', 'Export Shapefile'
+  it_behaves_like 'export geofile to local', 'berkeley-s7038h-shapefile.zip', 'Export Shapefile'
   it_behaves_like 'export geofile to local',  'berkeley-s7038h-kmz.kmz', 'Export KMZ'
   it_behaves_like 'export geofile to local',  'berkeley-s7038h-geojson.json', 'Export GeoJSON'
-  # after do
-  #   # FileUtils.rm_f(zip_file_path)
-  #   FileUtils.rm_f(shapefile_zip_file_path)
-  # end
 
-  # context 'export geo files' do
-  #   before do
-  #     find('#downloads-button').click
-  #   end
-  #   it 'displays the download button' do
-  #     find_link('Export Shapefile').click
-  #     sleep 5
-  #     expect(File.exist?(shapefile_zip_file_path)).to be_truthy, "Expected source data zip file not found: #{shapefile_zip_file_path}"
-  #     # sleep(500)
-  #   end
-
-  # end
 end
--- spec/support/shared_examples/link_spec.rb
+++ spec/support/shared_examples/link_spec.rb
@@ -23,25 +23,14 @@ RSpec.shared_examples 'export geofile to local' do |file_name, link_name|
   let(:download_dir) { '/opt/app/tmp/cache/downloads' }
   let(:export_file_path) { File.join(download_dir, file_name) }
 
-  before do
-    # FileUtils.rm_f(shapefile_zip_file_path)
-    FileUtils.mkdir_p(download_dir)
-    # Dir.mkdir(download_dir) unless File.exist?(download_dir)
-    # FileUtils.rm_f(shapefile_zip_file_path)
-    # FileUtils.mkdir_p(download_dir)
-    visit 'catalog/berkeley-s7038h'
-    find('#downloads-button').click
-  end
-
   after do
-    # FileUtils.rm_f(zip_file_path)
     FileUtils.rm_f(export_file_path)
   end
 
   it "export geofle: #{file_name}" do
     find_link(link_name).click
     sleep 5
-    expect(File.exist?(export_file_path)).to be_truthy, "Expected no exported geofile not found: #{export_file_path}"
+    expect(File.exist?(export_file_path)).to be_truthy, "Error: no exported geofile not found - #{export_file_path}"
   end
 
 end
