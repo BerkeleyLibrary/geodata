5bc29f936c78fe6b33b79db5a4bbb8fde9f2ea4a
zhouyu <zhouyu@berkeley.edu>
2025-01-15 15:45:56 -0800

    clear up session test

--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -1,23 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# # Get a test user, then testing Devise
+# Do not get new_user_session_path and user_calnet_omniauth_authorize_path anymore
+# Use a system test in 'restricted_access_spec.rb'
 
-# RSpec.describe 'Sessions' do
-#   # not use this action anymore
-#   # it 'can redirect Calnet login form' do
-#   #   get new_user_session_path
-#   #   expect(response).to have_http_status(302)
-#   # end
+RSpec.describe 'Calnet logout' do
 
-#   it 'can redirect Calnet logout form' do
-#     get destroy_user_session_path
-#     expect(response).to have_http_status(302)
-#   end
+  it 'can redirect Calnet logout form' do
+    get destroy_user_session_path
+    expect(response).to have_http_status(302)
+  end
 
-#   # not use redirection anymore
-#   # it 'redirects to auth.berkeley.edu' do
-#   #   get user_calnet_omniauth_authorize_path
-#   #   expect(response).to have_http_status(302)
-#   #   expect(response.location).to match(%r{https://auth-test.berkeley.edu})
-#   # end
-# end
+end
