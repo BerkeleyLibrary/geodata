9ebde5a2aaad8318b30d06096eac14a69a2a7d50
zhouyu <zhouyu@berkeley.edu>
2025-01-28 14:36:18 -0800

    update and clean up selenium download setup

--- spec/support/constants/test_constants.rb
+++ spec/support/constants/test_constants.rb
@@ -1,8 +1,6 @@
 module CommonHelpers
   EXPORT_TMP_PATH = '/opt/app/tmp/cache/downloads'.freeze
-  # DOWNLOAD_TMP_PATH = '/opt/app/tmp/selenium_downloads'.freeze
   DOWNLOAD_TMP_PATH = '/home/seluser/Downloads'.freeze
-  # DOWNLOAD_TMP_PATH = '/opt/app/artifacts'.freeze
   PUBLIC_RECORD_ID = 'berkeley-s7038h'.freeze
   RESTRICTED_RECORD_ID = 'berkeley-s7b12n'.freeze
 end
