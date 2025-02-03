cee96c991fc63491583ed56e744d87a5d17b6e6b
zhouyu <zhouyu@berkeley.edu>
2025-02-03 14:52:07 -0800

    Configure /opt/app/tmp/selenium_downloads as the shared volume for Selenium downloads

--- spec/support/constants/test_constants.rb
+++ spec/support/constants/test_constants.rb
@@ -1,6 +1,6 @@
 module CommonHelpers
   EXPORT_TMP_PATH = '/opt/app/tmp/cache/downloads'.freeze
-  DOWNLOAD_TMP_PATH = '/home/seluser/Downloads'.freeze
+  DOWNLOAD_TMP_PATH = '/opt/app/tmp/selenium_downloads'.freeze
   PUBLIC_RECORD_ID = 'berkeley-s7038h'.freeze
   RESTRICTED_RECORD_ID = 'berkeley-s7b12n'.freeze
 end
