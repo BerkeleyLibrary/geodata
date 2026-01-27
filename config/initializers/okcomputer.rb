a3af94a0720b2792d2e6ffecf2cd02785dbfbe3c
zhouyu <zhouyu@berkeley.edu>
2026-01-27 09:59:22 -0800

    Updated to store health check endpoint urls in environment variables

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -2,7 +2,6 @@
 # Health checks configuration
 
 require_relative '../../lib/http_head_check'
-require_relative '../../lib/endpoint_url'
 
 OkComputer.logger = Rails.logger
 OkComputer.check_in_parallel = true
@@ -16,13 +15,13 @@ core_baseurl = Blacklight.default_index.connection.uri.to_s.chomp('/')
 OkComputer::Registry.register 'solr', OkComputer::SolrCheck.new(core_baseurl)
 
 # Perform a Head request to check geoserver endpoint
-geoserver_url = EndpointUrl.geoserver(:geoserver)
+geoserver_url = Rails.configuration.x.servers[:geoserver]
 OkComputer::Registry.register 'geoserver', GeoDataHealthCheck::HttpHeadCheck.new(geoserver_url)
 
 # Perform a Head request to check secure_geoserver endpoint
-geoserver_secure_url = EndpointUrl.geoserver(:geoserver_secure)
+geoserver_secure_url = Rails.configuration.x.servers[:geoserver_secure]
 OkComputer::Registry.register 'geoserver_secure', GeoDataHealthCheck::HttpHeadCheck.new(geoserver_secure_url)
 
 # Perform a Head request to check spatial server endpoint
-spatial_server_url = EndpointUrl.spatial_server(:spatial_server)
+spatial_server_url = Rails.configuration.x.servers[:spatial_server]
 OkComputer::Registry.register 'spatial_server', GeoDataHealthCheck::HttpHeadCheck.new(spatial_server_url)
