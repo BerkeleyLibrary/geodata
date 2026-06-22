be2819cb66b6cc6371bb49c476e8481232833b7f
zhouyu <zhouyu@berkeley.edu>
2026-06-22 13:28:22 -0700

    add back ominiauth call back and session

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,32 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # Sets session data following successful Calnet login
+  def calnet
+    check_auth_params
+    calnet_sign_in
+    redirect_to params[:url] || root_path
+  rescue StandardError => e
+    logger.error "Calnet | ERROR: #{e.inspect}"
+    flash[:error] = t('omniauth.calnet.failure')
+    redirect_to root_path
+  end
+
+  private
+
+  def calnet_sign_in
+    user = User.from_calnet(auth_params)
+    Rails.logger.info(user.calnet_uid)
+    sign_in user
+  end
+
+  def auth_params
+    request.env['omniauth.auth']
+  end
+
+  def check_auth_params
+    Rails.logger.debug({
+      message: 'Received omniauth calnet callback',
+      omniauth_params: auth_params
+    }.to_json)
+  end
+
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+
+  def destroy
+    sign_out current_user if signed_in?
+    redirect_to calnet_logout_url, allow_other_host: true
+  end
+end
