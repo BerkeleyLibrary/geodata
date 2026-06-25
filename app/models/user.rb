62207bb0d897635df548580fe86e0037acf11563
zhouyu <zhouyu@berkeley.edu>
2026-06-25 14:31:06 -0700

    keep calnet old code for reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,31 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:cas]
 
+  # old code for omniauth, kept for reference
+  # class << self
+  #   # Finds or creates a user record given data from the omniauth request hash
+  #   def from_calnet(auth)
+  #     where(calnet_uid: auth.uid).first_or_initialize do |user|
+  #       user.email = auth.extra['berkeleyEduOfficialEmail'] || fake_email if user.email.blank?
+  #       user.save!
+  #     end
+  #   end
+
+  #   private
+
+  #   # Generates a random email address
+  #   def fake_email
+  #     "fake-#{SecureRandom.hex[0, 16]}@noemail.com"
+  #   end
+  # end
+
+  # # Method added by Blacklight; Blacklight uses #to_s on your
+  # # user class to get a user-displayable login/identifier for
+  # # the account.
+  # def to_s
+  #   email
+  # end
+
   # Configuration added by Blacklight; Blacklight::User uses a method key on your
   # user class to get a user-displayable login/identifier for
   # the account.
