dbaec11e3c18189ec26d54c09f8623ad9f5f0873
Jason Raitz <raitz@berkeley.edu>
2026-07-13 14:22:02 -0400

    WIP commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:cas]
+         :omniauthable, omniauth_providers: [:calnet]
 
   # old code for omniauth, kept for reference
   # class << self
