00348358451c44eb13324c58c3eea7952af8a01b
Daniel Schmidt <danschmidt5189@berkeley.edu>
2026-07-21 09:10:34 -0700

    Removes TimedHealthCheck wrapper

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -6,22 +6,16 @@ require_relative '../../lib/geo_data_health_check'
 OkComputer.logger = Rails.logger
 OkComputer.check_in_parallel = true
 
-health_check_timeout = 0.9
-
-timed_health_check = ->(check) do
-  GeoDataHealthCheck::TimedHealthCheck.new(check, timeout: health_check_timeout)
-end
-
-OkComputer::Registry.register 'default', timed_health_check.call(OkComputer::Registry.fetch('default'))
-OkComputer::Registry.register 'database', timed_health_check.call(OkComputer::Registry.fetch('database'))
+OkComputer::Registry.register 'default', OkComputer::Registry.fetch('default')
+OkComputer::Registry.register 'database', OkComputer::Registry.fetch('database')
 
 # Check that DB migrations have run
-OkComputer::Registry.register 'database-migrations', timed_health_check.call(OkComputer::ActiveRecordMigrationsCheck.new)
+OkComputer::Registry.register 'database-migrations', OkComputer::ActiveRecordMigrationsCheck.new
 
 # Check the Solr connection
 # Requires the ping handler on the solr core (<core>/admin/ping).
 core_baseurl = Blacklight.default_index.connection.uri.to_s.chomp('/')
-OkComputer::Registry.register 'solr', timed_health_check.call(OkComputer::SolrCheck.new(core_baseurl, 1))
+OkComputer::Registry.register 'solr', OkComputer::SolrCheck.new(core_baseurl, 1)
 
 {
   geoserver: Rails.configuration.x.servers[:geoserver],
@@ -30,5 +24,5 @@ OkComputer::Registry.register 'solr', timed_health_check.call(OkComputer::SolrCh
 }.each do |name, url|
   next if url.blank?
 
-  OkComputer::Registry.register name.to_s, timed_health_check.call(GeoDataHealthCheck::HttpHeadCheck.new(url, health_check_timeout))
+  OkComputer::Registry.register name.to_s, GeoDataHealthCheck::HttpHeadCheck.new(url, 0.9)
 end
