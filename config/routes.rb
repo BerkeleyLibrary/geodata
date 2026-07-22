8fd2e99fa17f8bb9c4a56e7ce7e94adc081de59a
zhouyu <zhouyu@berkeley.edu>
2026-07-21 19:03:15 -0700

    session view with http post

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_scope :user do
+    get 'users/sign_in', to: 'sessions#new', as: :new_user_session
+    get 'users/sign_out', to: 'sessions#destroy', as: :destroy_user_session
+  end
 
   concern :exportable, Blacklight::Routes::Exportable.new
 
