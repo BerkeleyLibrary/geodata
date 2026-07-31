cc6ff65d56eb8ce7eaff77664966a5b81da923b6
zhouyu <zhouyu@berkeley.edu>
2026-07-31 13:20:20 -0700

    fix the login link in navabar

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-# OmniAuth request phase defaults to POST in newer versions.
-# Allow GET as well so direct navigation to /users/auth/calnet works.
-OmniAuth.config.allowed_request_methods = %i[get post]
