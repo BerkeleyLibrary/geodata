67879691b8e936a7b8cfdb8fd720bd9c5b6c4b94
zhouyu <zhouyu@berkeley.edu>
2026-08-03 09:30:06 -0700

    replace download template helper with overlayed viewcomponets and with the same user seesioin flow

---
+++ app/components/geoblacklight/document/sidebar_component.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Geoblacklight
+  module Document
+    class SidebarComponent < Blacklight::Document::SidebarComponent
+      def render_static_map_component
+        render Geoblacklight::StaticMapComponent.new(document:)
+      end
+
+      def sidebar_buttons
+        [
+          Geoblacklight::WebServicesLinkComponent.new(document:),
+          Geoblacklight::DownloadLinksComponent.new(document:),
+          Geoblacklight::CalnetLoginLinkComponent.new(document:)
+        ]
+      end
+
+      def render_more_like_this
+        render Blacklight::Document::MoreLikeThisComponent.new(document:)
+      end
+    end
+  end
+end
