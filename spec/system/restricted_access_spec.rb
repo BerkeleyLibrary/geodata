e00adb375a6b3a409291fde4092c936147bc7bae
Jason Raitz <145712254+jason-raitz@users.noreply.github.com>
2026-08-07 12:04:37 -0400

    AP-815 Change Login Links to Buttons (#107)
    * AP-815 Change Login Links to Buttons
    
    Co-authored-by: Anna Wilcox <AWilcox@Wilcox-Tech.com>

--- spec/system/restricted_access_spec.rb
+++ spec/system/restricted_access_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe 'View Restricted Data', js: true do
   end
 
   it 'display login to view and download link' do
-    expect(page).to have_link('Login to View and Download')
+    expect(page).to have_button('Login to View and Download')
   end
 
   it 'open calnet login page when clicking login to view and download link' do
-    click_link('Login to View and Download')
+    click_button('Login to View and Download')
     expect(page).to have_current_path('/cas/login', ignore_query: true)
     expect(decoded_url(page.current_url)).to eq(cas_url)
   end
