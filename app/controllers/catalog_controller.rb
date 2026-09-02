8808f126554f32c8692cec5ade1dc2ad5bf964a4
Daniel Schmidt <danschmidt5189@berkeley.edu>
2026-09-02 11:19:23 -0700

    AP-872: Switch to openstreetmapStandard base map provider

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -285,7 +285,7 @@ class CatalogController < ApplicationController
     # 'openstreetmapHot'
     # 'openstreetmapStandard'
 
-    config.basemap_provider = 'positron'
+    config.basemap_provider = 'openstreetmapStandard'
 
     # Configuration for autocomplete suggestor
     config.autocomplete_enabled = true
